18f51c7c049ba4b30ba26a51decdf5c69b6619cd
dnhldbrndt <dnlhldbrndt@gmail.com>
2024-07-14 10:41:06 +0200

    Update NoteForm.tsx, App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,30 @@ import { Container } from "react-bootstrap"
 import { Routes, Route, Navigate } from "react-router-dom"
 import { NewNote } from "./NewNote"
 
+export type Note = {
+  id: string
+  & NoteData
+}
+export type NoteData = {
+  title: string
+  markdown: string
+  tags: Tag[]
+}
+export type Tag = {
+  id: string
+  label: string
+}
+export type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
+}
+export type RawNote = {
+  id: string
+}
 function App() {
- 
-
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+  const [notes, setNotes] = useLocalStorage<Tag[]>("TAGS", [])
   return ( <Container className="my-4">
     <Routes>
     <Route   path="/" element={<h1>Hi</h1>}></Route>
